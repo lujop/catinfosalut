810868c64c2666d1fda6a05cd998bbaf1d07477b
Joan Pujol <joanpujol@gmail.com>
2022-06-19 23:09:21 +0200

    Fix Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,7 +71,7 @@ resource "aws_api_gateway_method" "method" {
 resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = aws_api_gateway_rest_api.CatInfoSalutAPI.id
   resource_id             = aws_api_gateway_resource.hello.id
-  http_method             = aws_api_gateway_method.method
+  http_method             = aws_api_gateway_method.method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.catinfosalut.invoke_arn
