20cf3a079aabebb1cf8e1df9c4e92cc029a15b1a
Joan Pujol <joanpujol@gmail.com>
2022-06-19 22:57:35 +0200

    Fix terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,7 +58,7 @@ resource "aws_api_gateway_rest_api" "CatInfoSalutAPI" {
 resource "aws_api_gateway_resource" "hello" {
   parent_id   = aws_api_gateway_rest_api.CatInfoSalutAPI.root_resource_id
   path_part   = "resource"
-  rest_api_id = aws_api_gateway_rest_api.CatInfoSalutAPI
+  rest_api_id = aws_api_gateway_rest_api.CatInfoSalutAPI.id
 }
 
 resource "aws_api_gateway_method" "method" {
