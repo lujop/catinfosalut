bf6d151dbbd94c07bc1695f48869f96edd352aa1
Joan Pujol <joanpujol@gmail.com>
2022-06-19 22:48:26 +0200

    Create API Gateway to integrate with lambda function

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,6 +11,9 @@ provider "aws" {
   region = var.aws_region
 }
 
+###
+### Function deployment
+###
 resource "aws_iam_role" "lambda_exec" {
   name = "serverless_lambda"
 
@@ -44,3 +47,43 @@ resource "aws_lambda_function" "catinfosalut" {
     }
   }
 }
+
+###
+### API Gateway integration with function
+###
+resource "aws_api_gateway_rest_api" "CatInfoSalutAPI" {
+  name = "CatInfoSalutAPI"
+}
+
+resource "aws_api_gateway_resource" "hello" {
+  parent_id   = aws_api_gateway_rest_api.CatInfoSalutAPI.root_resource_id
+  path_part   = "resource"
+  rest_api_id = aws_api_gateway_rest_api.CatInfoSalutAPI
+}
+
+resource "aws_api_gateway_method" "method" {
+  authorization = "NONE"
+  http_method   = "GET"
+  rest_api_id   = aws_api_gateway_rest_api.CatInfoSalutAPI.id
+  resource_id   = aws_api_gateway_resource.hello.id
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = aws_api_gateway_rest_api.CatInfoSalutAPI.id
+  resource_id             = aws_api_gateway_resource.hello.id
+  http_method             = aws_api_gateway_method.method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.catinfosalut.invoke_arn
+}
+
+resource "aws_lambda_permission" "api_gateway_catinfosalut" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.catinfosalut.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  source_arn = "arn:aws:execute-api:${var.aws_region}:${var.aws_account_id}:${aws_api_gateway_rest_api.CatInfoSalutAPI.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.hello.path}"
+}
+
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,8 @@ variable "aws_region" {
   description = "AWS region"
   type        = string
 }
+
+variable "aws_account_id" {
+  description = "AWS account Id"
+  type        = string
+}
