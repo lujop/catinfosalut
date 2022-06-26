87d10f7e1aeded3bf97d9b46d71497ec99462352
Joan Pujol <joanpujol@gmail.com>
2022-06-26 19:47:03 +0200

    Enable cloudwatch log and Xray for function

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,21 +14,32 @@ provider "aws" {
 ###
 ### Function deployment
 ###
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+
 resource "aws_iam_role" "lambda_exec" {
   name = "serverless_lambda"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Sid    = ""
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-      }
-    ]
-  })
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+# Attach policy to be able log
+resource "aws_iam_role_policy_attachment" "basic" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.lambda_exec.name
+}
+# Attach policy to use XRay
+resource "aws_iam_role_policy_attachment" "aws_xray_write_only_access" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSXrayWriteOnlyAccess"
 }
 
 resource "aws_lambda_function" "catinfosalut" {
